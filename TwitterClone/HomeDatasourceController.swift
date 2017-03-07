87d8e5a966cd24d5337820131fd3cddb8431e523
Joe Kim <josephkim0224@gmail.com>
2017-03-07 18:46:35 -0500

    Dynamic cell sizing implemented

--- TwitterClone/HomeDatasourceController.swift
+++ TwitterClone/HomeDatasourceController.swift
@@ -9,6 +9,7 @@
 import LBTAComponents
 
 class HomeDatasourceController: DatasourceController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,8 +18,26 @@ class HomeDatasourceController: DatasourceController {
         
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
     override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 150)
+        
+        if let user = self.datasource?.item(indexPath) as? User {
+            
+            // estimation of height of cell based on bioText's size
+            
+            let approximateWidthOfBioTextView = view.frame.width - 12 - 50 - 12 - 2
+            let size = CGSize(width: approximateWidthOfBioTextView, height: 1000)
+            let attributes = [NSFontAttributeName: UIFont.systemFont(ofSize: 15)]
+            
+            let estimatedFrame = NSString(string: user.bioText).boundingRect(with: size, options: .usesLineFragmentOrigin, attributes: attributes, context: nil)
+            
+            return CGSize(width: view.frame.width, height: estimatedFrame.height + 20 + 12 + 20 + 12)
+        }
+        
+        return CGSize(width: view.frame.width, height: 200)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
